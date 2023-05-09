716ce3742da7e7387c80a688f576e9510e7df940
Stefizard <stefizards@gmail.com>
2023-05-09 17:32:49 +0300

    Added script to rotate camera

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Camera cam;
+    float camRotation = 0f;
+    float camRotationThisFrame, xMouse, yMouse;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xMouse = Input.GetAxis("Mouse X");
+        yMouse = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.up * xMouse * Time.deltaTime * 200);
+
+        camRotationThisFrame = yMouse * Time.deltaTime * 200;
+        if (camRotation + camRotationThisFrame > 90)
+        {
+            camRotationThisFrame = 90 - camRotation;
+        }
+        else if (camRotation + camRotationThisFrame < -90)
+        {
+            camRotationThisFrame = -(90 + camRotation);
+        }
+        cam.transform.Rotate(Vector3.left * camRotationThisFrame);
+        camRotation += camRotationThisFrame;
     }
 }
